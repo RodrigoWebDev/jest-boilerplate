6fc6b82aae371c6431198aaba27f52f6db6795e9
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2020-02-08 22:24:27 -0300

    added watch

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,3 +3,7 @@ import sum from "./sum"
 test(" sum(1,2) should return 3 ", () => {
     expect(sum(1,2)).toBe(3)
 })
+
+test(" sum(1,2) should not return 4 ", () => {
+    expect(sum(1,2)).not.toBe(4)
+})
